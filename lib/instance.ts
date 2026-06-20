30c916e438f21c8b6c9ee9a14180be90d01c08a2
SvendNeumann <svend.neumann@orisus.de>
2026-06-20 13:53:44 +0200

    feat: add single-instance MVZ configuration

---
+++ lib/instance.ts
@@ -0,0 +1,6 @@
+export const INSTANCE_NAME = "Zahnmedizin Westpfalz MVZ";
+
+export const INSTANCE_LOCATIONS = ["Ulmet", "Landstuhl", "Lauterecken"];
+
+export const INSTANCE_MODEL_NOTE =
+  "Diese App ist eine einzelne MVZ-Instanz. Die Softwarebasis kann kopiert werden, aber Datenbanken, Benutzer, Aufgaben, Checklisten, Nachweise und Historie bleiben je MVZ vollständig getrennt.";
